c8bc0ad3f207fd1d6a31b35d79bf98b3052537e8
saichii <overy1399@gmail.com>
2022-03-08 23:52:42 +0800

    頁尾版權文字

--- back/bottom.php
+++ back/bottom.php
@@ -1,22 +1,22 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-    <p class="t cent botli">網站標題管理</p>
-    <form method="post" target="back" action="?do=tii">
+    <p class="t cent botli">頁尾版權資料管理</p>
+    <form method="post" action="api/bottom.php">
         <table width="100%">
             <tbody>
                 <tr class="yel">
-                    <td width="45%">網站標題</td>
-                    <td width="23%">替代文字</td>
-                    <td width="7%">顯示</td>
-                    <td width="7%">刪除</td>
-                    <td></td>
+                    <td width="45%">頁尾版權資料: </td>
+                    <td width="45%"><input type="text" name="bottom" id="bottom"></td>
+
                 </tr>
             </tbody>
         </table>
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <td width="200px"><input type="button" onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;view.php?do=title&#39;)" value="新增網站標題圖片"></td>
-                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+                    <td class="cent">
+                        <input type="submit" value="修改確定">
+                        <input type="reset" value="重置">
+                    </td>
                 </tr>
             </tbody>
         </table>
